18bad8d7a8d56c852b6cd47e2252986198939467
Qingyunyijiu <116447850+Qingyunyijiu@users.noreply.github.com>
2023-08-02 23:28:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 // 替换为你的 webhook URL
 const WEBHOOK_URL = 'https://webhook.site/fbda7154-9421-4ab1-8570-a3eabbc605af';
 
-async function getLatestTemp() {
+// 获取温度数据的函数 
+async function getLatestTemp(){
+
   const response = await fetch(WEBHOOK_URL, {
-    method: 'POST' 
+    method: 'GET' 
   });
 
   const data = await response.json();
 
-  return data.temperature; 
+  return data.temperature;
+
 }
 
+// 每10秒调用函数获取最新温度
 setInterval(async () => {
+
   const temp = await getLatestTemp();
+
   document.getElementById('temp').innerText = temp;
+
 }, 10000);
